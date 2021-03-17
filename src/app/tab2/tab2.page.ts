a121bf9d0f8bcc9ce443a0cce9448413883b4bc9
m26416141 <m26416141@john.petra.ac.id>
2021-03-18 05:24:29 +0800

    tab-2 updated

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  private randomnumber : number;
+  guess : number;
+  correct : boolean = false;
+
   constructor() {}
+
+  numbergenerator() {
+    this.randomnumber = Math.floor((Math.random() * 10) + 1);
+    // this.randomnumber = 1
+    console.log(this.randomnumber);
+  }
+
+  iscorrect() {
+    if (this.guess == this.randomnumber) {
+      this.correct = true;
+    } else {
+      this.correct = false;
+    }
+  }
 
 }
